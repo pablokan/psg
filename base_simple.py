9fc44a6f14c19b6b3c6af13110c121afb4524d37
Pablo Kaniefsky <pablokan@gmail.com>
2021-07-27 16:06:21 -0300

    third

---
+++ base_simple.py
@@ -0,0 +1,21 @@
+import PySimpleGUI as sg
+
+def layout(): # Define la interfaz grafica
+    # Ejemplo de Pantalla
+    lista = [
+        [sg.Text('Nombre: ', size=(10, 1)), sg.Input(key='nombre')], 
+        [sg.Text('Edad: ', size=(10, 1)), sg.Input(key='edad')], 
+        [sg.Button('Ok', bind_return_key=True)]        
+        ]
+    return lista
+
+def main(window):
+    while True:
+        event, values = window.read()
+        if event in (sg.WIN_CLOSED, 'Quit'): break
+        print(event, values)
+        
+if __name__ == '__main__':
+    w = sg.Window('Title', layout(), location=(2600,330),resizable=True)
+    main(w)
+    w.close()
---
+++ psg_min.py
@@ -0,0 +1,4 @@
+import PySimpleGUI as sg
+w = sg.Window('Title', [[sg.Button('Click me')]])
+w.read()
+w.close()
